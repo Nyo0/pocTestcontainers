df6085dea53f95ee355cbb245c8ee41fc925d68d
leonardo_jobs@live.com <leonardo_jobs@live.com>
2023-04-05 03:45:22 -0300

    Commit inicial

--- src/main/java/com/example/poc/PocTestContainersApplication.java
+++ src/main/java/com/example/poc/PocTestContainersApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CleanArchApplication {
+public class PocTestContainersApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CleanArchApplication.class, args);
+		SpringApplication.run(PocTestContainersApplication.class, args);
 
 	}
 
